96c5418bf9e95332cc6c7b592599f80dca9c48ba
Patriach Cain <youanybluesky10@gmail.com>
2024-12-08 13:32:32 +0800

    added player

--- game.py
+++ game.py
@@ -2,69 +2,69 @@ import sys
 
 import pygame
 
+from scripts.utils import load_image
+from scripts.entities import PhysicsEntity
+
+
 class Game:
     def __init__(self):
-        #Initialize Pygame
+        # Initialize Pygame
         pygame.init()
-        #Change window name
+        # Change window name
         pygame.display.set_caption('Kuromi Legends')
-        #Change window resolution
+
+        # Change window resolution
         self.screen = pygame.display.set_mode((640, 480))
-        #initialize game clock
+
+        # Initialize second surface for rendering (used for asset scaling)
+        self.display = pygame.Surface((320, 240))
+
+        # initialize game clock
         self.clock = pygame.time.Clock()
 
-        #added cloud
-        self.img = pygame.image.load('data/images/clouds/cloud_1.png')
-        #added color key for transparency (black)
-        self.img.set_colorkey((0, 0, 0))
-        self.img_pos = [160, 260]
-        #Movement variable
+        # Movement variable
         self.movement = [False, False]
-        #Collision Area
-        self.collision_area = pygame.Rect(50, 50, 300, 50)
+
+        # Initialize Assets
+        self.assets = {
+            'player': load_image('entities/player.png')
+        }
+
+        # Define Player
+        self.player = PhysicsEntity(self, 'player', (50, 50), (8, 15))
 
     def run(self):
         while True:
-            #Clear the Screen
-            self.screen.fill((14, 219, 248))
+            # Clear the Screen
+            self.display.fill((14, 219, 248))
 
-            #Check if the image collided with the self.collision_area set
-            img_r = pygame.Rect(self.img_pos[0], self.img_pos[1], self.img.get_width(), self.img.get_height())
-            if img_r.colliderect(self.collision_area):
-                pygame.draw.rect(self.screen, (0, 100, 255), self.collision_area)
-            else:
-                pygame.draw.rect(self.screen, (0, 50, 155), self.collision_area)
+            # Update pos
+            self.player.update((self.movement[1] - self.movement[0], 0))
+            # Render Player
+            self.player.render(self.display)
 
-            self.img_pos[1] += (self.movement[1] - self.movement[0]) * 5
-            self.screen.blit(self.img, self.img_pos)
-
-            #Loop for All type of Events
+            # Loop for All type of Events
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
                 if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_UP:
+                    if event.key == pygame.K_LEFT:
                         self.movement[0] = True
-                    if event.key == pygame.K_DOWN:
-                        self.movement[1] = True
-                    if event.key == pygame.K_w:
-                        self.movement[0] = True
-                    if event.key == pygame.K_s:
+                    if event.key == pygame.K_RIGHT:
                         self.movement[1] = True
                 if event.type == pygame.KEYUP:
-                    if event.key == pygame.K_UP:
+                    if event.key == pygame.K_LEFT:
                         self.movement[0] = False
-                    if event.key == pygame.K_DOWN:
-                        self.movement[1] = False
-                    if event.key == pygame.K_w:
-                        self.movement[0] = False
-                    if event.key == pygame.K_s:
+                    if event.key == pygame.K_RIGHT:
                         self.movement[1] = False
 
-            #Method to update the screen every frame
+            # Blit the display into the screen
+            self.screen.blit(pygame.transform.scale(self.display, self.screen.get_size()), (0, 0))
+            # Method to update the screen every frame
             pygame.display.update()
             self.clock.tick(60)
 
-#Initialize the Game
+
+# Initialize the Game
 Game().run()
---
+++ scripts/entities.py
@@ -0,0 +1,23 @@
+import pygame
+
+
+class PhysicsEntity:
+    def __init__(self, game, e_type, pos, size):
+        self.game = game
+        self.type = e_type
+        self.pos = list(pos)
+        self.size = size
+        self.velocity = [0, 0]
+
+    # Update player position
+    def update(self, movement=(0, 0)):
+        # Vector to determine how much an entity should move in a frame
+        frame_movement = (movement[0] + self.velocity[0], movement[1] + self.velocity[1])
+
+        # Position for x and y dimensions
+        self.pos[0] += frame_movement[0]
+        self.pos[1] += frame_movement[1]
+
+    # Render entity
+    def render(self, surf):
+        surf.blit(self.game.assets['player'], self.pos)
