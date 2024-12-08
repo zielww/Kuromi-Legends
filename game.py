5a1468429197fc93eef1c2dd982c764539b3b321
Patriach Cain <youanybluesky10@gmail.com>
2024-12-08 13:05:15 +0800

    added comments

--- game.py
+++ game.py
@@ -2,25 +2,33 @@ import sys
 
 import pygame
 
-
 class Game:
     def __init__(self):
+        #Initialize Pygame
         pygame.init()
+        #Change window name
         pygame.display.set_caption('Kuromi Legends')
+        #Change window resolution
         self.screen = pygame.display.set_mode((640, 480))
+        #initialize game clock
         self.clock = pygame.time.Clock()
 
+        #added cloud
         self.img = pygame.image.load('data/images/clouds/cloud_1.png')
+        #added color key for transparency (black)
         self.img.set_colorkey((0, 0, 0))
         self.img_pos = [160, 260]
+        #Movement variable
         self.movement = [False, False]
-
+        #Collision Area
         self.collision_area = pygame.Rect(50, 50, 300, 50)
 
     def run(self):
         while True:
+            #Clear the Screen
             self.screen.fill((14, 219, 248))
 
+            #Check if the image collided with the self.collision_area set
             img_r = pygame.Rect(self.img_pos[0], self.img_pos[1], self.img.get_width(), self.img.get_height())
             if img_r.colliderect(self.collision_area):
                 pygame.draw.rect(self.screen, (0, 100, 255), self.collision_area)
@@ -30,6 +38,7 @@ class Game:
             self.img_pos[1] += (self.movement[1] - self.movement[0]) * 5
             self.screen.blit(self.img, self.img_pos)
 
+            #Loop for All type of Events
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
@@ -53,8 +62,9 @@ class Game:
                     if event.key == pygame.K_s:
                         self.movement[1] = False
 
+            #Method to update the screen every frame
             pygame.display.update()
             self.clock.tick(60)
 
-
+#Initialize the Game
 Game().run()
