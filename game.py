0e55417628f0311d5f31946b58acac3d07cd98f5
Patriach Cain <youanybluesky10@gmail.com>
2024-12-08 15:34:43 +0800

    added clouds

--- game.py
+++ game.py
@@ -6,6 +6,7 @@ from PIL.ImageChops import offset
 from scripts.utils import load_image, load_images
 from scripts.entities import PhysicsEntity
 from scripts.tilemap import Tilemap
+from scripts.clouds import Clouds
 
 
 class Game:
@@ -33,8 +34,13 @@ class Game:
             'grass': load_images('tiles/grass'),
             'large_decor': load_images('tiles/large_decor'),
             'stone': load_images('tiles/stone'),
-            'player': load_image('entities/player.png')
+            'player': load_image('entities/player.png'),
+            'background': load_image('background.png'),
+            'clouds': load_images('clouds')
         }
+
+        # Define Clouds
+        self.clouds = Clouds(self.assets['clouds'], count=16)
 
         # Define Player
         self.player = PhysicsEntity(self, 'player', (50, 50), (8, 15))
@@ -48,13 +54,17 @@ class Game:
     def run(self):
         while True:
             # Clear the Screen
-            self.display.fill((14, 219, 248))
+            self.display.blit(self.assets['background'], (0, 0))
 
             # Position the camera in the center of the screen (player)
             self.scroll[0] += (self.player.rect().centerx - self.display.get_width() / 2 - self.scroll[0]) / 30
             self.scroll[1] += (self.player.rect().centery - self.display.get_height() / 2 - self.scroll[1]) / 30
             # Remove jittery shit
             render_scroll = (int(self.scroll[0]), int(self.scroll[1]))
+
+            # Render Clouds
+            self.clouds.update()
+            self.clouds.render(self.display, offset=render_scroll )
 
             # Render tile map
             self.tilemap.render(self.display, offset=render_scroll)
